a6d187796222286d83cb6d3287cefa7ee7b82bb9
HAJI <gwd7454@gmail.com>
2021-09-15 11:38:08 +0100

    update metadata.js

--- src/metadata/metadata.js
+++ src/metadata/metadata.js
@@ -6,7 +6,7 @@ module.exports = {
     LinkedIn: '<a href="http://www.linkedin.com/in/haji8"><i class="fab fa-linkedin fact-icon"></i> haji8 </a>',
     GitHub: '<a href="https://github.com/haji8-data"><i class="fab fa-github fact-icon"></i> haji8-data </a>',
     Email: '<a href="mailto:gwd7454@gmail.com">gwd7454@gmail.com</a>',
-    Korean_CV:'<a href="https://haji8-data.github.io/cv/">https://haji8-data.github.io/cv_eng/</a>',
+    'Korean CV':'<a href="https://haji8-data.github.io/cv/"><i class="fab fa-file-text-o fact-icon"></i>CV_Kor</a>',
 
   },
   skills: [
